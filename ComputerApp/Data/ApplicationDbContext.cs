09f88172d7c8cf207d5906bcc9cf9e331e19f100
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-03-14 15:09:49 +0100

    Identity Added and some change to include AppUser. Just before migration

--- ComputerApp/Data/ApplicationDbContext.cs
+++ ComputerApp/Data/ApplicationDbContext.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ComputerApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ComputerApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
