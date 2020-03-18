e1a38935765f1654324fb358e275138f57cdc7b0
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-03-18 22:31:09 +0100

    BuildComputer JS Updated

--- ComputerApp/wwwroot/js/BuildComputer.js
+++ ComputerApp/wwwroot/js/BuildComputer.js
@@ -6,6 +6,15 @@ const sel = document.getElementsByTagName('select');
 const totalPriceTag = document.getElementById("total-price");
 // console.log(sel);
 
+for (let index = 0; index < sel.length; index++) {
+    let num = +sel[index].options[sel[index].selectedIndex].getAttribute("price");
+    listPrices[index] = num;
+    //getSelectedOption();
+}
+totalPrice = listPrices.reduce((a, b) => a + b, 0);
+totalPriceTag.innerHTML = `Total Price: <strong>€ ${totalPrice.toFixed(2)}</strong>`;
+
+
 const calcTotalPrice = (index) => {
     let num = +sel[index].options[sel[index].selectedIndex].getAttribute("price");
     listPrices[index] = num;
@@ -16,10 +25,8 @@ for (let index = 0; index < sel.length; index++) {
     sel[index].addEventListener("click", () => {
         let num = +sel[index].options[sel[index].selectedIndex].getAttribute("price");
         listPrices[index] = num;
-        let totalPrice = listPrices.reduce((a, b) => a + b, 0);
+        totalPrice = listPrices.reduce((a, b) => a + b, 0);
         totalPriceTag.innerHTML = `Total Price: <strong>€ ${totalPrice.toFixed(2)}</strong>`;
         console.log(totalPrice);
     });
 }
-
-
